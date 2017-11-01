87ef29724cd30b46d7bfc694ce55d07f2d800d14
Anessa Arnold <anessaarnold@icloud.com>
2017-11-01 11:55:05 -0700

    remove watcher from config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    
   config.paperclip_defaults = {
     storage: :s3,
     s3_protocol: 'http',
