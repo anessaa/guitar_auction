6b23fc55aea67f2f15cf01418fb1f1901e72c236
Anessa Arnold <anessaarnold@icloud.com>
2017-11-01 12:18:52 -0700

    add user fk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101184653) do
+ActiveRecord::Schema.define(version: 20171101191555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 20171101184653) do
     t.bigint "guitar_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["guitar_id"], name: "index_comments_on_guitar_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "guitars", force: :cascade do |t|
@@ -61,5 +63,6 @@ ActiveRecord::Schema.define(version: 20171101184653) do
 
   add_foreign_key "bids", "guitars"
   add_foreign_key "comments", "guitars"
+  add_foreign_key "comments", "users"
   add_foreign_key "guitars", "users"
 end
